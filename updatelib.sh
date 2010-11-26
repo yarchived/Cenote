9ffbdb8dec1bd72faa7fa9fe76909089e3363873
yaroot <yaroot@gmail.com>
2010-11-26 17:31:53 +0800

    include libs

---
+++ updatelib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+function co()
+{
+    if [ -d libs/$2 ]; then
+        svn update libs/$2
+    else
+        svn co $1 libs/$2
+    fi
+}
+
+if [ ! -d libs ]; then
+    mkdir libs
+fi
+co 'svn://svn.wowace.com/wow/ace3/mainline/trunk/LibStub' 'LibStub'
+co 'svn://svn.wowace.com/wow/ace3/mainline/trunk/CallbackHandler-1.0' 'CallbackHandler-1.0'
+co 'svn://svn.wowace.com/wow/ace3/mainline/trunk/AceAddon-3.0' 'AceAddon-3.0'
+
+
